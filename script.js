b18e60ab93797645cf09f0efd1d7b251f73ac4ce
Gautam Krishna.M <73305324+gautam-gk@users.noreply.github.com>
2021-08-27 15:16:22 +0530

    Just Begine the js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+//Selector
+const todoinput = document.querySelector('.todo_list_input');
+const todobutton = document.querySelector('.todo_input_button');
+const todolist = document.querySelector('.todo_list');
+
+
+
+//Event Listner
+todobutton.addEventListener('click', addTodo);
+
+
+
+
+//Function
+function addTodo(event) {
+    event.preventDefault();
+    console.log('hi');
+
+};
 
